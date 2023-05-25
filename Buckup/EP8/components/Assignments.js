c56a69844f91fa170d04a2dd569762e57c22a327
Taha Mohamed <taha.mohamed2016@gmail.com>
2023-05-25 14:04:16 +0300

    EP7, 8, 9 and 10

---
+++ Buckup/EP8/components/Assignments.js
@@ -0,0 +1,47 @@
+import AssignmentList from "./AssignmentList.js";
+
+export default {
+    components:{
+        AssignmentList
+    },
+    template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            <form @submit.prevent="add">
+                <div class="border border-gray-600 text-black">
+                    <input v-model="newAssignment" placeholder="Type Assignment..." class="p-2">
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
+        </section>
+    `,
+    data() {
+        return {
+            assignments: [
+                {name: 'Finish project', completed: false, id: 1},
+                {name: 'Read chapter 4', completed: false, id: 2},
+                {name: 'Turn in homework', completed: false, id: 3},
+            ],
+            newAssignment: ''
+        }
+    },
+    computed: {
+        filters(){
+            return{
+                inProgress: this.assignments.filter(assignment => !assignment.completed),
+                completed: this.assignments.filter(assignment => assignment.completed)
+            }
+        }
+    },
+   methods: {
+        add(){
+            this.assignments.push({
+                name: this.newAssignment,
+                completed: false,
+                id: this.assignments.length + 1
+            });
+            this.newAssignment = ''
+        }
+   }
+}
